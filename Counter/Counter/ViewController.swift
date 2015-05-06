44d53d80fa2934e0048219909e4aa7c459580b2b
tutujiaw <tujiaw@163.com>
2015-05-06 22:20:08 +0800

    xxxxxx

---
+++ Counter/Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by tutujiaw on 15/5/6.
+//  Copyright (c) 2015年 tutujiaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
